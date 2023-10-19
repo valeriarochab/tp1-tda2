327d84958ee7e81f85057f6d38383688a13aa86b
valeriarochab <valeria.rocha@despegar.com>
2023-10-18 23:38:36 -0300

    add jupyter notebook

--- main.py
+++ main.py
@@ -14,21 +14,29 @@ def main():
     print(graph)
 
 
-def graph_diameter():
+def graph_diameter(graph):
     max_diameter = 0
-    for row in graph:
-        diameter = len(bfs(graph, row))
-        if diameter > max_diameter:
-            max_diameter = diameter
+    for node in graph:
+        distances = bfs(graph, node)
+        max_distance = max(distances.values())
+        max_diameter = max(max_diameter, max_distance)
+    return max_diameter
 
 def bfs(graph, start):
     visited = set()
-    queue = deque([start])
+    queue = deque([(start, 0)])
+    distances = {start: 0}
+
     while queue:
-        node = queue.popleft()
+        node, dist = queue.popleft()
+
         if node not in visited:
             visited.add(node)
-            queue.extend(graph[node] - visited)
-    return visited
+            for neighbor in graph.get(node, []):
+                if neighbor not in distances:
+                    distances[neighbor] = dist + 1
+                    queue.append((neighbor, dist + 1))
+
+    return distances
 
 main()
